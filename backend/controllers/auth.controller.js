e47dad4c136d8ac7eb836fc207d978b17bbb63fe
Heshan Rathnapala <43751413+hrathnapala@users.noreply.github.com>
2025-02-03 23:27:25 +0400

    mongo db integration and user saving part implementation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,21 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req,res) =>{
-    res.send("Signup Called");
-};
+    const {email, password, name} = req.body;
+    try {
+        const userExists = await User.findOne({email});
+
+        if(userExists) {
+            return res.status(400).json({message:  "User is already exists"})
+        }
+    
+        const user = await User.create({email,password,name});
+    
+        return res.status(201).json({user,message: "User created successfully"}); 
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+}
 
 export const login = async (req,res) =>{
     res.send("Login Called");
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 
+//db connect
+import {connectDB} from './lib/db.js';
+
 //routes
 import authRoutes from "./routes/auth.route.js";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 9001; 
 
+
+app.use(express.json());
+
 //authentication
 app.use("/api/auth", authRoutes);
 
 app.listen(9000,() => {
     console.log(`server is running on port  ${PORT}`);
+    connectDB();
 });
